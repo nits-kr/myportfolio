0f01f934fa335cbb03b90159290cd22510405e0d
Nitish Kumar <nits.kr99@gmail.com>
2026-01-25 13:12:00 +0530

    Add dark/light theme toggle

--- frontend/src/app/projects/page.js
+++ frontend/src/app/projects/page.js
@@ -26,7 +26,6 @@ export default function ProjectsPage() {
   console.log("user", user);
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
     setIsMounted(true);
   }, []);
 
