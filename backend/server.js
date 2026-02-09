ad121871c00684d46548f204e1cbfdb8fee349a2
Nitish Kumar <nits.kr99@gmail.com>
2026-02-09 22:30:11 +0530

    blogs create claude

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
+import "dotenv/config";
 import express from "express";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import helmet from "helmet";
--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,7 +1,12 @@
+import "dotenv/config";
 import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv";
 
-dotenv.config();
+const mask = (str) =>
+  str ? `${str[0]}****${str[str.length - 1]}` : "undefined";
+console.log("Cloudinary Config Check:");
+console.log("- Cloud Name:", mask(process.env.CLOUDINARY_CLOUD_NAME));
+console.log("- API Key:", mask(process.env.CLOUDINARY_API_KEY));
+console.log("- API Secret:", mask(process.env.CLOUDINARY_API_SECRET));
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
